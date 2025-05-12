23886d03a457340c83011ed1ee70451eb687d365
Clarisse Rodrigues <98330252+clarodriguess@users.noreply.github.com>
2025-05-12 12:22:11 -0300

    Delete src directory

--- src/Pessoa.java
+++
@@ -1,8 +0,0 @@
-//hora da pratica
-//1.1 Crie uma classe Pessoa com um metodo que exibe "Olá, mundo!" no console.
-
-public class Pessoa {
-    void mansagem(){
-        System.out.println("Ola, mundo!");
-    }
-}
